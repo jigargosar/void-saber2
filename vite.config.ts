72ad2f504ebaffd82c4a9574a2f56b961728c2b4
Jigar Gosar <jigar.gosar@gmail.com>
2026-03-03 09:38:40 +0530

    added ip host exposed on network

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [tailwindcss(), basicSsl()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: false,
+    },
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+    },
 })
